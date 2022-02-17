d5f38d4aa4b3c89ff269316aa4af7b0ce01efae8
YaserObeid <yaser.obeid@hotmail.com>
2022-02-17 18:59:58 +0100

    create Controller

--- src/main/java/com/obeid/springboot/thymleafdemo/controller/EmployeeController.java
+++ src/main/java/com/obeid/springboot/thymleafdemo/controller/EmployeeController.java
@@ -3,15 +3,33 @@ package com.obeid.springboot.thymleafdemo.controller;
 
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.obeid.springboot.thymleafdemo.entity.Employee;
+import com.obeid.springboot.thymleafdemo.service.EmployeeService;
 
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 	
+	private EmployeeService employeeService;
+	
 	// Get: employees/list
+	
+	@GetMapping("/list")
+	public String findAll(Model model) {
+		List<Employee> employees = employeeService.findAll();
+		
+		model.addAttribute("employees", employees);
+		
+		return "employees/employee-list";
+	}
 	
 	
 	
