28296dceae345976f67ba4593712e9f272dc5571
YaserObeid <yaser.obeid@hotmail.com>
2022-02-17 19:42:35 +0100

    test list & update

--- src/main/java/com/obeid/springboot/thymleafdemo/controller/EmployeeController.java
+++ src/main/java/com/obeid/springboot/thymleafdemo/controller/EmployeeController.java
@@ -5,6 +5,7 @@ package com.obeid.springboot.thymleafdemo.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import com.obeid.springboot.thymleafdemo.service.EmployeeService;
 @RequestMapping("/employees")
 public class EmployeeController {
 	
+	@Autowired
 	private EmployeeService employeeService;
 	
 	// Get: employees/list
